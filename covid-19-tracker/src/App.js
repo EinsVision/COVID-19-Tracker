81237c6d80885e3e06ee5a6c5f1449f6e38b4de1
EinsVision <einsvision@gmail.com>
2021-02-25 02:54:55 +0900

    Installed material ui icons and core

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -4,6 +4,17 @@ function App() {
   return (
     <div className="app">
       <h1>COVID-19-TRACKER 🚀✨🎈⭐</h1>
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+      {/* InfoBoxs */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }

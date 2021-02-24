0a0168331fa598c00bb90cfd7683542a5b2ce140
EinsVision <einsvision@gmail.com>
2021-02-25 03:00:46 +0900

    dropdown menu

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,9 +1,21 @@
   import './App.css';
+  import { FormControl, MenuItem, Select } from '@material-ui/core';
 
 function App() {
   return (
     <div className="app">
       <h1>COVID-19-TRACKER 🚀✨🎈⭐</h1>
+      <FormControl className='app__dropdown'>
+        <Select
+          variant='outlined'
+          value='abc'
+        >
+          <MenuItem value='worldwide'>Worldwide</MenuItem>
+          <MenuItem value='worldwide'>Option two</MenuItem>
+          <MenuItem value='worldwide'>Option three</MenuItem>
+          <MenuItem value='worldwide'>Option four</MenuItem>
+        </Select>
+      </FormControl>
       {/* Header */}
       {/* Title + Select input dropdown field */}
 

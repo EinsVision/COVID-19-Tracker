593a2f665ac6be097998360ad650117dcd068606
EinsVision <einsvision@gmail.com>
2021-02-25 21:56:30 +0900

    represent flag and styling for the info box

--- covid-19-tracker/src/util.js
+++ covid-19-tracker/src/util.js
@@ -56,13 +56,13 @@ export const showDataOnMap = (data, casesType='cases') => (
           ></div>
           <div className="info-name">{country.country}</div>
           <div className="info-confirmed">
-            Cases: {numeral(country.cases).format("0,0")}
+            <strong>Cases:</strong> {numeral(country.cases).format("0,0")}
           </div>
           <div className="info-recovered">
-            Recovered: {numeral(country.recovered).format("0,0")}
+            <strong>Recovered:</strong> {numeral(country.recovered).format("0,0")}
           </div>
           <div className="info-deaths">
-            Deaths: {numeral(country.deaths).format("0,0")}
+            <strong>Deaths:</strong> {numeral(country.deaths).format("0,0")}
           </div>
         </div>
       </Popup>

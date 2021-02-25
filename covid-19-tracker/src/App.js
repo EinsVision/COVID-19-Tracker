4895597c3bd6517061542f114d6716f8cdc4aa1e
EinsVision <einsvision@gmail.com>
2021-02-25 14:43:03 +0900

    npm install --save react-chartjs-2 chart.js

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -5,6 +5,7 @@ import InfoBox from './InfoBox';
 import Map from './Map';
 import Table from './Table';
 import { sortData } from './util';
+import LineGraph from './LineGraph';
 
 // https://disease.sh/v3/covid-19/countries (data)
 function App() {
@@ -117,7 +118,7 @@ function App() {
           <h3>Live Cases by Country</h3>
           <Table countries={tableData} />
           <h3>Worldwide new cases</h3>
-          {/* Graph */}
+          <LineGraph />
         </CardContent>
       </Card>
     </div>
